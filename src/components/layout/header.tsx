79d6f26d051c3627ac14fc425efb695fff7508ea
= <=>
2025-03-03 17:08:29 +0600

    style: implements basic header layout styles

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,26 @@
+import { ThemeSwitch } from "../theme";
 import { Container } from "./container";
+import Link from "next/link";
+import { homePath } from "@/paths";
 
 export default function Header() {
   return (
-    <header className="">
-      <Container>Header</Container>
+    <header className="relative py-2">
+      <Container>
+        <div className="flex justify-between items-center">
+          <div className="">
+            <Link
+              href={homePath()}
+              className="font-black tracking-wider text-xl underline"
+            >
+              BDSTORE
+            </Link>
+          </div>
+          <div className="">
+            <ThemeSwitch />
+          </div>
+        </div>
+      </Container>
     </header>
   );
 }
