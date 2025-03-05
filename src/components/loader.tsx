aba97e781d8856688918f191747cace6500b6c53
Tauhid <tauhidahmed716@gmail.com>
2025-03-05 20:41:55 +0600

    style(loader): section gap increase

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -3,7 +3,7 @@ import { Section } from "./layout/section";
 
 export default function PageLoader() {
   return (
-    <Section className="flex-1 flex items-center justify-center">
+    <Section className="flex-1 flex items-center justify-center" padding="lg">
       <LucideLoaderCircle className="animate-spin" />
     </Section>
   );
