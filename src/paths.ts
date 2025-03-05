51c3a64755fb0179353dabaa4e60bb55f91714c9
Tauhid <tauhidahmed716@gmail.com>
2025-03-05 16:55:59 +0600

    refactor(paths): rename routes from login to signin

--- src/paths.ts
+++ src/paths.ts
@@ -1,4 +1,4 @@
 export const homePath = () => "/";
-export const loginPath = () => "/auth/login";
-export const registerPath = () => "/auth/register";
+export const loginPath = () => "/signin";
+export const registerPath = () => "/register";
 export const redirectPath = () => "/";
