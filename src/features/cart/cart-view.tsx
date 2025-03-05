03109dedc32ed9bb5e1e8f45708d1b129e284aeb
Tauhid <tauhidahmed716@gmail.com>
2025-03-05 22:51:19 +0600

    feat(cart): initial cart overview component add

---
+++ src/features/cart/cart-view.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { LucideShoppingCart } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+
+export default function CartView() {
+  return (
+    <div>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button className="!size-9 relative" variant="outline">
+            <LucideShoppingCart />
+            <span className="absolute right-0 top-0 -translate-y-2 translate-x-2">
+              <Badge className="size-5 rounded-full inline-flex items-center justify-center shadow">
+                9+
+              </Badge>
+            </span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+}
