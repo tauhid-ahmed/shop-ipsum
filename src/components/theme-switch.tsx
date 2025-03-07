5b867c57c2264507b4348663aa2c1908bb62f633
Tauhid <tauhidahmed716@gmail.com>
2025-03-07 14:25:26 +0600

    refactor(theme): refactor theme switch

---
+++ src/components/theme-switch.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import * as React from "react";
+import * as SwitchPrimitive from "@radix-ui/react-switch";
+
+import { cn } from "@/lib/utils";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+export function ThemeSwitch({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof SwitchPrimitive.Root>) {
+  const { setTheme, resolvedTheme } = useTheme();
+  const [isDark, setIsDark] = React.useState(resolvedTheme === "dark");
+  React.useEffect(() => {
+    setIsDark(resolvedTheme === "dark");
+  }, [resolvedTheme]);
+
+  const toggleTheme = () => {
+    setTheme(isDark ? "light" : "dark");
+  };
+  return (
+    <SwitchPrimitive.Root
+      checked={isDark}
+      onCheckedChange={toggleTheme}
+      data-slot="switch"
+      className={cn(
+        "peer focus-visible:border-ring focus-visible:ring-ring/50 inline-flex h-5 w-10 shrink-0 items-center rounded-full border-1 border-rose-400 dark:border-cyan-500 shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 cursor-pointer",
+        className
+      )}
+      {...props}
+    >
+      <SwitchPrimitive.Thumb
+        data-slot="switch-thumb"
+        className={cn(
+          "pointer-events-none flex items-center justify-center size-4 rounded-full ring-0 shadow-lg !transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
+        )}
+      >
+        {isDark ? (
+          <Moon className="!size-3.5 text-cyan-500" />
+        ) : (
+          <Sun className="!size-3.5 text-rose-500" />
+        )}
+      </SwitchPrimitive.Thumb>
+    </SwitchPrimitive.Root>
+  );
+}
