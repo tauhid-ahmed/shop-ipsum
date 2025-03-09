53dc237f6d37bbb067b0b2b71bca2f478f3f49b1
Tauhid <tauhidahmed716@gmail.com>
2025-03-09 16:09:25 +0600

    auth schema improves

--- src/features/auth/auth-schema.ts
+++ src/features/auth/auth-schema.ts
@@ -3,11 +3,13 @@ import z from "zod";
 const PASSWORD_LENGTH = 4;
 
 export const LoginSchema = z.object({
-  email: z.string().email(),
+  email: z
+    .string({ required_error: "Email is required" })
+    .email("Email is required"),
   password: z
-    .string()
-    .min(PASSWORD_LENGTH, {
-      message: `Password must be at least ${PASSWORD_LENGTH} characters long`,
+    .string({ required_error: "Password is required" })
+    .min(1, {
+      message: "Password is required",
     })
     .max(128, { message: "Password cannot exceed 128 characters" })
     .regex(/[A-Z]/, {
@@ -25,11 +27,29 @@ export const LoginSchema = z.object({
 });
 
 export const RegisterSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(PASSWORD_LENGTH, {
-    message: "Password must be at least 4 characters long",
-  }),
-  confirm_password: z.string().min(PASSWORD_LENGTH, {
-    message: "Password must be at least 4 characters long",
-  }),
+  email: z
+    .string({ required_error: "Email is required" })
+    .email("Email is required"),
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(PASSWORD_LENGTH, {
+      message: "Password must be at least 4 characters long",
+    })
+    .max(128, { message: "Password cannot exceed 128 characters" })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number" })
+    .regex(/[@$!%*?&]/, {
+      message: "Password must contain at least one special character (@$!%*?&)",
+    })
+    .trim(),
+  confirm_password: z
+    .string({ required_error: "Password does not match!" })
+    .min(PASSWORD_LENGTH, {
+      message: "Password must be at least 4 characters long",
+    }),
 });
