f84de4829af3604327f7ee15e183bcf3eed578c3
Tauhid <tauhidahmed716@gmail.com>
2025-03-12 01:27:57 +0600

    form message font size shrinks

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -80,7 +80,7 @@ function FormItem({ className, ...props }: React.ComponentProps<"div">) {
     <FormItemContext.Provider value={{ id }}>
       <div
         data-slot="form-item"
-        className={cn("grid gap-2", className)}
+        className={cn("grid gap-1", className)}
         {...props}
       />
     </FormItemContext.Provider>
@@ -148,7 +148,7 @@ function FormMessage({ className, ...props }: React.ComponentProps<"p">) {
     <p
       data-slot="form-message"
       id={formMessageId}
-      className={cn("text-destructive text-sm", className)}
+      className={cn("text-destructive text-xs font-medium", className)}
       {...props}
     >
       {body}
