ac9e9ac0bfcdeeccd7acae8f098e306e8fc167f3
Tauhid <tauhidahmed716@gmail.com>
2025-03-16 01:40:35 +0600

    profile picture size variants add

--- src/components/profile-image.tsx
+++ src/components/profile-image.tsx
@@ -1,17 +1,36 @@
+import { cn } from "@/lib/utils";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 
 const imagePath = "/favicon.ico";
 
-export default function ProfileImage() {
+type ProfileImageProps = {
+  size?: "default" | "md" | "lg";
+  className?: string;
+  align?: "center";
+};
+
+export default function ProfileImage({
+  size = "default",
+  align,
+}: ProfileImageProps) {
   const { data: session } = useSession();
+
   return (
-    <div className="size-8 rounded-full overflow-hidden">
+    <div
+      className={cn(`rounded-full overflow-hidden`, {
+        "size-8": size === "default",
+        "size-12": size === "md",
+        "size-14": size === "lg",
+        "mx-auto": align === "center",
+      })}
+    >
       <Image
         src={session?.user?.image || imagePath}
         width={40}
         height={40}
         alt={session?.user?.name || "Guest User"}
+        className="object-cover size-full"
       />
     </div>
   );
--- src/features/users/profile-widget.tsx
+++ src/features/users/profile-widget.tsx
@@ -14,17 +14,16 @@ import {
 } from "@/components/ui/popover";
 import { Heading } from "@/components/heading";
 import Link from "next/link";
-import { registerPath, signInPath } from "@/constants/paths";
+import { signInPath } from "@/constants/paths";
 import { ThemeSwitch } from "@/components/theme-switch";
 import { Button } from "@/components/ui/button";
 import React from "react";
-import Image from "next/image";
-import { type AuthUserType } from "@/features/auth/types";
 import { usePathname } from "next/navigation";
 import { useAuth } from "@/hooks/useAuth";
 import SignedIn from "@/components/signed-in";
 import SignedOut from "@/components/signed-out";
 import ProfileImage from "@/components/profile-image";
+import { signOut } from "next-auth/react";
 
 const menuItems = [
   {
@@ -52,8 +51,6 @@ const menuItems = [
 
 export default function ProfileWidget() {
   const [popoverOpen, setPopoverOpen] = React.useState(false);
-
-  const { user } = useAuth();
   const pathname = usePathname();
 
   React.useEffect(() => {
@@ -81,7 +78,7 @@ export default function ProfileWidget() {
           sideOffset={16}
           className="relative p-0 rounded text-muted-foreground"
         >
-          <ProfileHeader user={user as AuthUserType} />
+          <ProfileHeader />
           <div className="[&>*]:px-4 [&>*]:py-3 [&_svg]:size-5 text-sm font-medium divide-y divide-border">
             {menuItems.map((item) => (
               <Link
@@ -104,20 +101,12 @@ export default function ProfileWidget() {
   );
 }
 
-function ProfileHeader({ user }: { user: AuthUserType }) {
-  const guestUser = Boolean(!user.id);
-  const isUser = !guestUser;
+function ProfileHeader() {
+  const { user } = useAuth();
   return (
     <>
       <div className="flex flex-col gap-2 py-6 border-b border-border bg-accent/40">
-        <div className="size-10 mx-auto rounded-full border-2 border-primary overflow-hidden">
-          <Image
-            src={user.image as string}
-            alt={user.name}
-            width={40}
-            height={40}
-          />
-        </div>
+        <ProfileImage align="center" size="lg" />
         <div className="text-center">
           <Heading
             as="h3"
@@ -131,40 +120,17 @@ function ProfileHeader({ user }: { user: AuthUserType }) {
             {user.email}
           </p>
           <div className="flex gap-4 justify-center mt-2">
-            {isUser && <RegisteredUser />}
-            {guestUser && <GuestUser />}
+            <Button
+              className="text-destructive-light"
+              variant="secondary"
+              size="sm"
+              onClick={() => signOut()}
+            >
+              Sign Out
+            </Button>
           </div>
         </div>
       </div>
-    </>
-  );
-}
-
-function GuestUser() {
-  return (
-    <>
-      <Button size="sm" asChild>
-        <Link href={signInPath()}>Sign In</Link>
-      </Button>
-      <Button size="sm" variant="outline" asChild>
-        <Link href={registerPath()}>Register</Link>
-      </Button>
-    </>
-  );
-}
-
-function RegisteredUser() {
-  const { signOut } = useAuth();
-  return (
-    <>
-      <Button
-        className="text-destructive-light"
-        variant="secondary"
-        size="sm"
-        onClick={() => signOut()}
-      >
-        Sign Out
-      </Button>
     </>
   );
 }
