4526d10be2b0c39facfa5c0ca3cdc4e23ac68bdd
Tauhid <tauhidahmed716@gmail.com>
2025-03-16 13:25:45 +0600

    add vercel lib

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,24 @@
-import { NextResponse } from "next/server";
-// import { auth } from "./auth.config";
-// import * as paths from "@/constants/paths";
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./auth.config";
+import * as paths from "@/constants/paths";
 
-// export default async function middleware(req: NextRequest) {
-export default async function middleware() {
-  // const session = await auth();
-  // const { nextUrl } = req;
-  // const isLoggedIn = !!session?.user;
+export default async function middleware(req: NextRequest) {
+  const session = await auth();
+  const { nextUrl } = req;
+  const isLoggedIn = !!session?.user;
 
-  // const isPublicRoute = paths.publicRoutes.includes(nextUrl.pathname);
-  // const isAuthRoute = paths.authRoutes.includes(nextUrl.pathname);
+  const isPublicRoute = paths.publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = paths.authRoutes.includes(nextUrl.pathname);
 
-  // if (nextUrl.pathname.startsWith("/api/auth")) return null;
+  if (nextUrl.pathname.startsWith("/api/auth")) return null;
 
-  // if (isLoggedIn && isAuthRoute) {
-  //   return NextResponse.redirect(new URL(paths.defaultRedirectPath(), nextUrl));
-  // }
+  if (isLoggedIn && isAuthRoute) {
+    return NextResponse.redirect(new URL(paths.defaultRedirectPath(), nextUrl));
+  }
 
-  // if (!isLoggedIn && !isPublicRoute && !isAuthRoute) {
-  //   return NextResponse.redirect(new URL(paths.signInPath(), nextUrl));
-  // }
-
-  return NextResponse.next();
+  if (!isLoggedIn && !isPublicRoute && !isAuthRoute) {
+    return NextResponse.redirect(new URL(paths.signInPath(), nextUrl));
+  }
 }
 
 export const config = {
