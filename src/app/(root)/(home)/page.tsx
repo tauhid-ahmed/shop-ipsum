b006bd46fdb9798099845db6b7a853ce49dae540
Tauhid <tauhidahmed716@gmail.com>
2025-03-18 03:31:54 +0600

    add footer

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -2,6 +2,9 @@ import { Section } from "@/components/layout/section";
 import { Container } from "@/components/layout/container";
 import Slider from "@/components/carousel/slider";
 import ProductReveal from "@/features/products/product-reveal";
+import { Heading } from "@/components/heading";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 const getRandomDuration = () =>
   Math.floor(Math.random() * (5000 - 3000 + 1)) + 3000;
@@ -26,6 +29,27 @@ export default async function Home() {
         speed={getRandomSpeed()}
         title="New Arrivals"
       />
+      <Section padding="lg">
+        <Container>
+          <div className="relative h-96 md:h-[500px] bg-accent/50 flex items-center justify-center px-4">
+            <div className="absolute inset-0 -z-10 opacity-20">
+              <Image src="/assets/section/section-01.jpg" fill alt="" />
+            </div>
+            <div className="flex flex-col gap-4 text-center">
+              <Heading size="5xl" weight="bold">
+                Get 25% off during our one-time sale
+              </Heading>
+              <p className="text-lg text-muted-foreground">
+                Most of our products are limited releases that won't come back.
+                Get your favorite items while they're in stock.
+              </p>
+              <div className="">
+                <Button size="lg">Get access to our one-time sale</Button>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </Section>
       <ProductReveal
         duration={getRandomDuration()}
         speed={getRandomSpeed()}
--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,9 +1,15 @@
+import { env } from "@/constants/env";
 import { Container } from "./container";
 
 export default function Footer() {
   return (
-    <footer>
-      <Container>Footer</Container>
+    <footer className="border-t border-border py-4 text-center">
+      <Container>
+        <p className="text-sm text-muted-foreground">
+          &copy; {new Date().getFullYear()} {env.NEXT_PUBLIC_APP_NAME}. All
+          rights reserved.
+        </p>
+      </Container>
     </footer>
   );
 }
