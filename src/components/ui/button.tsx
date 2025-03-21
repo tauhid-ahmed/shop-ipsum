90920209db9acaffd5e5c9102cf8f3a1350cb8f8
Tauhid <tauhidahmed716@gmail.com>
2025-03-21 12:25:10 +0600

    fix button height in sm screen

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -25,7 +25,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 gap-1.5 px-3 has-[>svg]:px-2.5",
         md: "h-9 gap-1.5 px-3.5 has-[>svg]:px-3",
-        lg: "h-9 md:h-10 px-6 has-[>svg]:px-4",
+        lg: "h-10 px-6 has-[>svg]:px-4",
         icon: "size-9",
       },
       shape: {
