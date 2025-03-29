e3ec4e100f5f011182254d80871356e14a0ae43c
Tauhid <tauhidahmed716@gmail.com>
2025-03-29 23:53:33 +0600

    implement initial attribute scrap from product data

--- src/hooks/useProductAttributes.ts
+++ src/hooks/useProductAttributes.ts
@@ -5,25 +5,19 @@ import { useEffect, useState } from "react";
 
 const defaultProduct = products[1];
 
-type AvailableAttribute<T extends string> = {
-  [K in T]: string;
-} & {
-  inStock: boolean;
-};
-
-type ColorAttribute = AvailableAttribute<"color">;
-type SizeAttribute = AvailableAttribute<"size">;
-
 export function useProductAttributes(product: ProductType = defaultProduct) {
   const [allColors, setAllColors] = useState<string[]>([]);
   const [allSizes, setAllSizes] = useState<string[]>([]);
-  const [availableColors, setAvailableColors] = useState<ColorAttribute[]>([]);
-  const [availableSizes, setAvailableSizes] = useState<SizeAttribute[]>([]);
+  const [availableColors, setAvailableColors] = useState<string[]>([]);
+  const [availableSizes, setAvailableSizes] = useState<string[]>([]);
+  const [selectedColor, setSelectedColor] = useState<string>("Blue");
+  const [selectedSize, setSelectedSize] = useState<string>("");
 
   const { inventory } = product;
 
+  // Initial select all colors.
   useEffect(() => {
-    const uniqueAttributes = inventory.variants.reduce(
+    const allUniqueAttributes = inventory.variants.reduce(
       (accumulatedAttributes, currentVariant) => {
         accumulatedAttributes.colors = [
           ...new Set([...accumulatedAttributes.colors, currentVariant.color]),
@@ -39,9 +33,42 @@ export function useProductAttributes(product: ProductType = defaultProduct) {
       }
     );
 
-    setAllColors(uniqueAttributes.colors);
-    setAllSizes(uniqueAttributes.sizes);
+    const availableUniqueAttributes = inventory.variants.reduce(
+      (accumulatedAttributes, currentVariant) => {
+        accumulatedAttributes.colors.push(
+          ...new Set([
+            ...accumulatedAttributes.colors,
+            currentVariant.inStock ? currentVariant.color : "",
+          ])
+        );
+        accumulatedAttributes.sizes.push(
+          ...new Set(
+            Object.entries(currentVariant.sizeStock)
+              .filter((item) => Boolean(item[1]))
+              .map((item) => item[0])
+          )
+        );
+        return accumulatedAttributes;
+      },
+      {
+        colors: [] as string[],
+        sizes: [] as string[],
+      }
+    );
+
+    setAllColors(allUniqueAttributes.colors);
+    setAllSizes(allUniqueAttributes.sizes);
+    setAvailableColors(availableUniqueAttributes.colors);
+    setAvailableSizes(availableUniqueAttributes.sizes);
   }, [inventory.variants]);
 
+  useEffect(() => {
+    const colors = inventory.variants.filter(
+      (variant) => variant.color === selectedColor && variant.inStock
+    );
+    console.log(colors);
+  }, [selectedColor]);
+
+  console.log({ allColors, allSizes, availableColors, availableSizes });
   return { allColors, allSizes };
 }
