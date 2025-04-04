527b95cf9c181ebab39c7b29218d2e833b0d10f8
Tauhid <tauhidahmed716@gmail.com>
2025-04-04 19:08:20 +0600

    improve theme preference settings -- check if existing theme clicks

--- src/actions/theme-preference.action.ts
+++ src/actions/theme-preference.action.ts
@@ -3,5 +3,9 @@ import { cookies } from "next/headers";
 
 export async function setThemePreference(theme: string) {
   const cookieStore = await cookies();
+  const currentTheme = cookieStore.get("preference")?.value;
+
+  if (currentTheme === theme) return;
+
   cookieStore.set("preference", theme);
 }
