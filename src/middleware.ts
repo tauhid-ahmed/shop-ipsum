771df62c8c9128af935f8783e65f8dc6cc4e3693
Tauhid <tauhidahmed716@gmail.com>
2025-04-22 03:11:57 +0600

    eslint fix unused vars

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 // import { auth } from "./auth.config";
 // import * as paths from "@/constants/paths";
 
