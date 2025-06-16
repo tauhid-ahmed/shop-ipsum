de015975982564534c24c989e8ffd4485bd4fbe2
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-16 20:07:51 +0600

    feat: add sendEmail route to Hono server and create routes index file

--- src/app/(server)/index.ts
+++ src/app/(server)/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { logger } from "hono/logger";
 import { cors } from "hono/cors";
+import { sendEmail } from "./routes/";
 
 // export const runtime = "edge";
 
@@ -9,7 +10,8 @@ const app = new Hono()
   .basePath("/api")
   // middlewares
   .use("*", logger())
-  .use("*", cors());
+  .use("*", cors())
+  .route("/send-email", sendEmail);
 
 export const GET = handle(app);
 export const POST = handle(app);
