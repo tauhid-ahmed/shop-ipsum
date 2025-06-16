394db9109b3094111b780e10f1e0ba203acca2e9
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-16 13:39:06 +0600

    feat: add user registration and sign-in actions with email verification

---
+++ src/actions/auth/index.ts
@@ -0,0 +1,4 @@
+export { registerUserAction } from "./register-user.action";
+export { signInAction } from "./signin.action";
+export { socialAction } from "./social.action";
+export { verifyEmailTokenAction } from "./verify-email.action";
---
+++ src/actions/auth/social.action.ts
@@ -0,0 +1,7 @@
+"use server";
+import { signIn } from "@/auth";
+import { defaultRedirectPath } from "@/constants/paths";
+
+export async function socialAction(provider: "google" | "github") {
+  await signIn(provider, { callbackUrl: defaultRedirectPath() });
+}
