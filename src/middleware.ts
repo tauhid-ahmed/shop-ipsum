75ffb461b756fbd7209ffc89d8f94191537365ef
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-17 22:56:12 +0600

    refactor: enhance middleware logic to include public route checks for authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,8 @@ import {
   defaultRedirectRoute,
   signInRoute,
   authApiRoutes,
+  publicRoutes,
+  homeRoute,
 } from "@/constants/paths";
 
 const adminRoles = ["admin", "super-admin"];
@@ -16,6 +18,9 @@ export default async function middleware(req: NextRequest) {
   const isLoggedIn = !!session?.user;
 
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isPublicRoute =
+    nextUrl.pathname === homeRoute() ||
+    publicRoutes.some((route) => nextUrl.pathname.startsWith(route));
   const isAdminRoute = nextUrl.pathname.startsWith(adminRoutes);
   const isAuthApiRoute = nextUrl.pathname.startsWith(authApiRoutes);
 
@@ -25,7 +30,7 @@ export default async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL(defaultRedirectRoute(), nextUrl));
   }
 
-  if (!isLoggedIn && !isAuthRoute) {
+  if (!isLoggedIn && !isAuthRoute && !isPublicRoute) {
     return NextResponse.redirect(new URL(signInRoute(), nextUrl));
   }
 
