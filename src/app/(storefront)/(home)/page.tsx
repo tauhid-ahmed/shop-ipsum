4f4a6fdd18d2143e4b8c2295636c64b043c3d237
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-18 03:13:07 +0600

    feat: add FeaturesGrid and NewsLetter components to Home page

--- src/app/(storefront)/(home)/page.tsx
+++ src/app/(storefront)/(home)/page.tsx
@@ -3,6 +3,7 @@ import IntroBanner from "@/components/banner/intro.banner";
 import ProductCategoryCard from "@/components/products/product-category-preview";
 import { ProductMarketingCallout } from "@/components/products";
 import { products } from "@/data/products";
+import { FeaturesGrid, NewsLetter } from "@/components";
 // import { getTrendingProducts } from "@/lib/product-ranking";
 
 export default async function Home() {
@@ -11,16 +12,22 @@ export default async function Home() {
   return (
     <>
       <IntroBanner />
+
       <ProductReveal title="Trending" products={products} />
+      <FeaturesGrid />
+
       <ProductReveal title="Featured" products={products} />
-      <ProductCategoryCard />
-      {/* <ProductReveal category="New Arrivals" /> */}
       <ProductMarketingCallout
         title="Get 25% off during our one-time sale"
         description="Most of our products are limited releases that won't come back. Get your favorite items while they're in stock."
         ctaLink="#"
         ctaText="Get access to our one-time"
       />
+      <ProductCategoryCard />
+
+      {/* <ProductReveal category="New Arrivals" /> */}
+
+      <NewsLetter />
       {/* <ProductReveal category="Best Sellers" /> */}
       {/* <ProductReveal category="Featured" /> */}
     </>
