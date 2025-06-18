2459820e83e5dce6b29f48d48cdb56f0a6b54e97
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-19 00:02:15 +0600

    feat: enhance UserProfileWidget with logged-in and guest user handling, improve appearance options

--- src/components/users/user-profile-widget.tsx
+++ src/components/users/user-profile-widget.tsx
@@ -1,19 +1,24 @@
 "use client";
-import { Heading } from "@/components";
-import { ThemeSwitch } from "@/components";
+import { Heading, ThemeSwitch } from "@/components";
 import { Button } from "@/components/ui/button";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { signInRoute } from "@/constants/paths";
+import { useAuth } from "@/hooks/useAuth";
 import {
   LucideGift,
   LucideHeart,
   LucideListOrdered,
+  LucideLogIn,
   LucideShoppingBag,
+  LucideSwatchBook,
   LucideUser,
+  LucideUser2,
 } from "lucide-react";
+import { User } from "next-auth";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -44,22 +49,13 @@ const menuItems = [
   { name: "Gift cards", icon: <LucideGift />, href: "#" },
 ];
 
-type ProfileWidgetProps = {
-  user: {
-    name: string;
-    email: string;
-    image?: string;
-  };
-};
-export default function UserProfileWidget({ user }: ProfileWidgetProps) {
+export default function UserProfileWidget({ user }: { user: User }) {
   const [popoverOpen, setPopoverOpen] = useState(false);
   const pathname = usePathname();
 
   useEffect(() => {
     setPopoverOpen(false);
   }, [pathname]);
-
-  if (!user) return;
 
   return (
     <Popover
@@ -69,68 +65,111 @@ export default function UserProfileWidget({ user }: ProfileWidgetProps) {
     >
       <PopoverTrigger className="cursor-pointer" asChild>
         <Button size="icon" variant="ghost">
-          <UserProfilePicture size="md" name={user.name} image={user.image} />
+          {user?.name && (
+            <UserProfilePicture size="md" name={user.name} image={user.image} />
+          )}
+          {!user?.name && <LucideUser2 />}
         </Button>
       </PopoverTrigger>
-      <PopoverContent
-        side="top"
-        sideOffset={20}
-        align="center"
-        className="relative p-0 text-sm rounded text-muted-foreground"
-      >
-        <ProfileHeader
-          name={user.name}
-          email={user.email}
-          image={user.image ?? ""}
-        />
-        <div className="[&>*]:px-4 [&>*]:py-3 [&_svg]:size-4 text-sm font-medium divide-y divide-border bg-accent/20">
-          {menuItems.map((item) => (
-            <Link
-              key={item.name}
-              className="flex items-center gap-2 cursor-pointer hover:bg-secondary/40"
-              href={item.href}
-            >
-              {item.icon}
-              {item.name}
-            </Link>
-          ))}
-          <div className="flex justify-between bg-accent/60">
-            <span>Appearance</span>
-            <ThemeSwitch />
-          </div>
-        </div>
+      <PopoverContent side="top" sideOffset={20} align="center">
+        <LoggedInProfileHeader />
+        <GuestProfileHeader />
+        <NavigationMenu />
       </PopoverContent>
     </Popover>
   );
 }
 
-function ProfileHeader({
-  name,
-  email,
-  image,
-}: {
-  name: string;
-  email: string;
-  image: string;
-}) {
+function NavigationMenu() {
   return (
-    <>
-      <div className="flex flex-col gap-2 py-6 border-b border-border bg-accent/60 items-center">
-        <UserProfilePicture size="lg" name={name} image={image} />
-        <Heading
-          as="h3"
-          size="lg"
-          weight="bold"
-          className="text-secondary-foreground capitalize"
-          align="center"
+    <div className="[&>*]:px-4 [&>*]:py-3 [&_svg]:size-4 text-sm font-medium divide-y divide-border bg-accent/20">
+      {menuItems.map((item) => (
+        <Link
+          key={item.name}
+          className="flex items-center gap-2 cursor-pointer hover:bg-secondary/40"
+          href={item.href}
         >
-          {name}
-        </Heading>
-        <p className="text-muted-foreground text-sm font-medium">{email}</p>
-        <Button size="sm" variant="destructive" onClick={() => signOut()}>
-          Sign Out
+          {item.icon}
+          {item.name}
+        </Link>
+      ))}
+      <AppearanceChange />
+      <SignOutButton />
+    </div>
+  );
+}
+
+function LoggedInProfileHeader() {
+  const { user } = useAuth();
+  if (!user) return;
+  return (
+    <div className="py-6 border-b border-border bg-accent/60 items-center">
+      {user.name ? (
+        <UserProfilePicture size="lg" name={user.name} image={user.image} />
+      ) : (
+        <LucideUser />
+      )}
+      <Heading
+        as="h3"
+        size="lg"
+        weight="bold"
+        className="text-secondary-foreground capitalize mt-4"
+        align="center"
+      >
+        {user.name}
+      </Heading>
+      <p className="text-muted-foreground text-sm font-medium text-center">
+        {user.email}
+      </p>
+    </div>
+  );
+}
+
+function GuestProfileHeader() {
+  const { user } = useAuth();
+  if (user) return null;
+  return (
+    <div className="py-6 border-b border-border bg-accent/60 items-center">
+      <LucideUser className="size-10 mx-auto text-foreground" />
+      <Heading
+        as="h3"
+        size="lg"
+        weight="bold"
+        className="text-secondary-foreground capitalize mt-2"
+        align="center"
+      >
+        Guest User
+      </Heading>
+      <div className="flex gap-2 justify-center mt-2">
+        <Button size="sm" asChild>
+          <Link href={signInRoute()}>
+            <LucideLogIn /> Sign in
+          </Link>
         </Button>
       </div>
-    </>
+    </div>
+  );
+}
+
+function AppearanceChange() {
+  return (
+    <div className="flex justify-between">
+      <span className="flex items-center gap-2 cursor-pointer hover:bg-secondary/40">
+        <LucideSwatchBook /> Appearance
+      </span>
+      <ThemeSwitch />
+    </div>
+  );
+}
+
+function SignOutButton() {
+  const { user } = useAuth();
+  if (!user) return null;
+  return (
+    <div className="bg-accent/50">
+      <Button onClick={() => signOut()} className="w-full" variant="outline">
+        Sign out
+      </Button>
+    </div>
   );
 }
