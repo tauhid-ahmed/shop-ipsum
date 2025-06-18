f823ccc3315b154de897dae10b4484f8f52eb7af
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-18 18:19:16 +0600

    refactor: update ThemeSwitch component for improved readability and structure

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -1,32 +1,39 @@
 "use client";
 
-import * as React from "react";
 import * as SwitchPrimitive from "@radix-ui/react-switch";
+import * as React from "react";
 
 import { cn } from "@/lib/utils";
-import { Moon, Sun } from "lucide-react";
+import { LucideSunMoon } from "lucide-react";
+import { motion } from "motion/react";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
-export function ThemeSwitch({
+export default function ThemeSwitch({
   className,
   ...props
 }: React.ComponentProps<typeof SwitchPrimitive.Root>) {
   const { setTheme, resolvedTheme } = useTheme();
-  const [isDark, setIsDark] = React.useState(resolvedTheme === "dark");
-  React.useEffect(() => {
+  const [isDark, setIsDark] = useState(resolvedTheme === "dark");
+
+  useEffect(() => {
     setIsDark(resolvedTheme === "dark");
   }, [resolvedTheme]);
 
   const toggleTheme = () => {
     setTheme(isDark ? "light" : "dark");
   };
+
   return (
     <SwitchPrimitive.Root
       checked={isDark}
       onCheckedChange={toggleTheme}
       data-slot="switch"
       className={cn(
-        "peer focus-visible:border-ring focus-visible:ring-ring/50 inline-flex h-5 w-10 shrink-0 items-center rounded-full border-1 border-rose-500 dark:border-cyan-500 shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 cursor-pointer",
+        "peer focus-visible:border-ring focus-visible:ring-ring inline-flex h-6 w-10 shrink-0 items-center rounded-full border-2 border-primary outline-none focus-visible:ring-px disabled:cursor-not-allowed disabled:opacity-50 cursor-pointer p-0.5",
+        isDark
+          ? "justify-end border-sky-600"
+          : "justify-start border-amber-600",
         className
       )}
       {...props}
@@ -34,14 +41,18 @@ export function ThemeSwitch({
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
         className={cn(
-          "pointer-events-none flex items-center justify-center size-3.5 rounded-full ring-0 shadow-lg !transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-1"
+          "pointer-events-none flex items-center justify-center size-4"
         )}
+        asChild
       >
-        {isDark ? (
-          <Moon className="text-sky-500" />
-        ) : (
-          <Sun className="text-rose-500" />
-        )}
+        <motion.div layout>
+          <LucideSunMoon
+            className={cn(
+              "stroke-3",
+              isDark ? "text-sky-600" : "text-amber-600"
+            )}
+          />
+        </motion.div>
       </SwitchPrimitive.Thumb>
     </SwitchPrimitive.Root>
   );
