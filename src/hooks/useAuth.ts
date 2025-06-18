a68c133060ef562a84430fe3401a123410c34045
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-19 00:01:54 +0600

    refactor: simplify user handling in useAuth hook for clarity

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,26 +1,20 @@
 "use client";
-
+import { User } from "next-auth";
 import { useSession, signIn, signOut } from "next-auth/react";
-
-type User = {
-  name?: string | null;
-  email?: string | null;
-  image?: string | null;
-};
 
 export function useAuth() {
   const { data: session, status } = useSession();
-  console.log({ session });
 
   const isLoading = status === "loading";
   const isAuthenticated = status === "authenticated";
-  const user: User | null = isAuthenticated ? session?.user ?? null : null;
 
-  return {
-    user,
-    isLoading,
-    isAuthenticated,
-    signIn,
-    signOut,
-  };
+  if (isAuthenticated) {
+    return {
+      user: session.user as User,
+      isLoading,
+      isAuthenticated,
+      signIn,
+      signOut,
+    };
+  } else return { user: null };
 }
