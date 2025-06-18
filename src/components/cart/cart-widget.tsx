2cedc63b58196adf1711bb849843ddebd600b001
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-19 01:17:59 +0600

    refactor: streamline CartWidget component structure and improve styling

--- src/components/cart/cart-widget.tsx
+++ src/components/cart/cart-widget.tsx
@@ -1,112 +1,133 @@
+import { Heading } from "@/components";
+import { Button } from "@/components/ui/button";
 import {
-  LucideChevronLeft,
+  Popover,
+  PopoverAnchor,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Separator } from "@/components/ui/separator";
+import placeHolderImage from "@/images/image-placeholder.svg";
+import {
   LucideMinus,
   LucidePlus,
   LucideShoppingBag,
   LucideX,
 } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverAnchor,
-} from "@/components/ui/popover";
-import { Heading } from "@/components";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import { productStatus } from "@/lib/productStatus";
-import { cn } from "@/lib/utils";
-import { Separator } from "@/components/ui/separator";
+import { Badge } from "../ui/badge";
 
 export function CartWidget() {
   return (
     <Popover>
       <PopoverTrigger asChild>
         <div className="flex items-center">
-          <Button size="icon" shape="pill" variant="ghost">
+          <Button size="icon" variant="ghost">
             <LucideShoppingBag />
           </Button>
-          <span className="text-sm">0</span>
+          <span className="text-sm">3</span>
         </div>
       </PopoverTrigger>
       <PopoverAnchor>
         <PopoverContent
           side="top"
-          align="end"
+          align="center"
           sideOffset={34}
-          className="w-80 flex flex-col h-[calc(100vh-8rem)] p-0 right-0"
+          className="w-80 flex flex-col max-h-[calc(100vh-4rem)] p-0 [&>*]:p-4 overflow-hidden rounded shadow-2xl"
         >
-          <CartHeader />
-          <div className="flex flex-col flex-1 [&>*]:p-4 overflow-hidden">
-            <div className="flex-1 overflow-y-scroll bg-accent/20">
-              <CartBody />
-            </div>
-            <CartFooter />
+          <CartWidgetHeader />
+          <div className="flex-1 overflow-y-scroll">
+            <CartBody />
           </div>
+          <CartFooter />
         </PopoverContent>
       </PopoverAnchor>
     </Popover>
   );
 }
 
-function CartHeader() {
+function CartWidgetHeader() {
   return (
-    <div className="border-b border-double border-border bg-accent/40 flex gap-1 items-center">
-      <PopoverTrigger className="flex items-center cursor-pointer font-normal py-4 text-sm">
-        <LucideChevronLeft size={20} />
-        <span>Continue Shopping</span>
-      </PopoverTrigger>
+    <div className="flex justify-between bg-accent/50 shadow">
+      <Heading as="h3" size="lg">
+        My Cart (3)
+      </Heading>
+      <Button variant="link" size="sm">
+        <Link href="/">View all</Link>
+      </Button>
     </div>
   );
 }
 
 function CartBody() {
   return (
-    <div className="flex flex-col gap-3">
-      {Array.from({ length: 9 }).map((_, i) => {
-        const { message, className } = productStatus.stock(
-          Math.floor(Math.random() * 15)
-        );
+    <div className="flex flex-col gap-8">
+      {Array.from({ length: 3 }).map((_, i) => {
         return (
           <React.Fragment key={i}>
             <div className="flex gap-4 relative">
-              <div className="h-24 w-20 bg-secondary"></div>
+              <Image
+                className="h-20 w-16 rounded mt-1 object-cover"
+                src={placeHolderImage}
+                alt="product image"
+              />
               <div className="flex flex-col gap-1 overflow-hidden">
-                <div className="overflow-hidden">
+                <div className="overflow-hidden space-y-2">
                   <Heading
                     className="text-ellipsis text-foreground/80"
                     align="left"
-                    as="h3"
-                    size="md"
-                    weight="semibold"
+                    as="h4"
+                    size="sm"
                   >
-                    Product
+                    Luxury
                   </Heading>
-                  <p className="text-xs font-medium text-muted-foreground">
-                    Description
-                  </p>
-                </div>
-                <div className="flex items-center justify-between gap-4">
-                  <div className="inline-flex align-middle rounded-lg border-t border-b border-border">
-                    <div className="flex -my-px">
-                      <Button className="size-8" size="icon" variant="outline">
-                        <LucideMinus />
-                      </Button>
-                      <div className="w-8 text-center text-sm flex items-center justify-center">
-                        55
-                      </div>
-                      <Button className="size-8" size="icon" variant="outline">
-                        <LucidePlus />
-                      </Button>
-                    </div>
+                  <Heading
+                    className="text-ellipsis text-foreground uppercase"
+                    align="left"
+                    as="h4"
+                    size="md"
+                  >
+                    Formal Jeans
+                  </Heading>
+                  <div className="text-xs space-x-2">
+                    <span>
+                      Size: <strong>XL</strong>
+                    </span>
+                    <span>
+                      Color: <strong>Purple</strong>
+                    </span>
                   </div>
-                  <p className="text-sm flex items-baseline gap-1">
-                    <strong className="font-medium">$ {(23).toFixed(2)}</strong>
-                  </p>
+                  <div className="space-x-2">
+                    <Badge className="bg-emerald-200 text-emerald-900">
+                      Free Shipping
+                    </Badge>
+                    <Badge className="bg-amber-200 text-amber-900">
+                      Disc 5%
+                    </Badge>
+                  </div>
+                  <div className="flex items-center justify-between gap-4">
+                    <div className="inline-flex align-middle rounded-lg border-t border-b border-border">
+                      <div className="flex -my-px">
+                        <Button size="icon" variant="outline">
+                          <LucideMinus />
+                        </Button>
+                        <div className="aspect-square h-9 border-t-1 border-b-1 border-border text-center text-sm flex items-center justify-center">
+                          55
+                        </div>
+                        <Button size="icon" variant="outline">
+                          <LucidePlus />
+                        </Button>
+                      </div>
+                    </div>
+                    <p className="text-sm flex items-baseline gap-1">
+                      <strong className="font-medium">
+                        $ {(23).toFixed(2)}
+                      </strong>
+                    </p>
+                  </div>
                 </div>
-                <p className={cn("text-xs text-ellipsis", className)}>
-                  {message}
-                </p>
               </div>
               <div className="absolute top-0 right-1">
                 <Button variant="ghost" size="icon">
@@ -123,7 +144,7 @@ function CartBody() {
 
 function CartFooter() {
   return (
-    <div className="border-t border-border bg-accent/90 flex flex-col gap-3">
+    <div className="border-t border-border bg-accent/50 flex flex-col gap-3 py-12!">
       <div className="flex items-center gap-4 pb-4 text-sm">
         <strong>Subtotal</strong>
         <Separator orientation="vertical" />
@@ -135,9 +156,6 @@ function CartFooter() {
         </span>
       </div>
       <Button className="w-full">Proceed to checkout</Button>
-      <Button variant="outline" className="w-full">
-        View your cart
-      </Button>
     </div>
   );
 }
