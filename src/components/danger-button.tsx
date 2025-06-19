0f216733d6c940873cfda5f4b3cbd4699b23f755
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-06-19 13:09:57 +0600

    feat: add DangerButton component for interactive button functionality

---
+++ src/components/danger-button.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "./ui/button";
+
+type DangerButtonProps = {
+  variant?: "ghost" | "default" | "outline" | "secondary";
+} & React.ComponentProps<"button">;
+
+export default function DangerButton({
+  variant = "ghost",
+  children,
+  ...props
+}: DangerButtonProps) {
+  const [isInteractive, setIsInteractive] = useState(false);
+  const handleInteractiveOn = () => setIsInteractive(true);
+  const handleInteractiveOff = () => setIsInteractive(false);
+  return (
+    <Button
+      onPointerDown={handleInteractiveOn}
+      onPointerEnter={handleInteractiveOn}
+      onPointerUp={handleInteractiveOff}
+      onPointerLeave={handleInteractiveOff}
+      variant={isInteractive ? "destructive" : variant}
+      size="sm"
+      className="absolute -top-1 right-1"
+      {...props}
+    >
+      {children}
+    </Button>
+  );
+}
